3eb66c86324a6e78d85abf9c8fb6c8de607b3b74
grantandrus <grantandrus2004@gmail.com>
2026-03-21 02:16:31 -0600

    Working on login logic

--- RVfamcamp/RVfamcamp/Pages/Login.cshtml.cs
+++ RVfamcamp/RVfamcamp/Pages/Login.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Runtime.InteropServices;
 
 namespace RVfamcamp.Pages
 {
@@ -7,6 +8,18 @@ namespace RVfamcamp.Pages
     {
         public void OnGet()
         {
+
         }
+        public IActionResult OnPost()
+        {
+            if (Request.Form["email_input"] == "temp@gmail" && Request.Form["password_input"] == "temp")
+            {
+                HttpContext.Session.SetString("User", "Exists");
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
+        }
+
     }
 }
--- RVfamcamp/RVfamcamp/wwwroot/js/site.js
+++ RVfamcamp/RVfamcamp/wwwroot/js/site.js
@@ -3,8 +3,4 @@
 
 // Commenting out for time being. Will revisit once login is flushed out. 
 
-//document.getElementById("loginLink").addEventListener("click", function() {
-//    doucment.getElementById("paymentsLink").removeAttribute("disabled");
-//    document.getElementById("reservationsLink").removeAttribute("disabled");
-//    document.getElementById("profileLink").removeAttribute("disabled");
-//});
+// Removed login stuff from js and transferred to login controller in C#
