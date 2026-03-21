a82bf8ffcc8b289cf8af7ffa9ac0e2515551b97c
Noah-Chisholm <nchisholm1006@gmail.com>
2026-03-21 16:50:52 -0600

    I have programed a good test of the stripe payment system. If you go to localhost:PORT/Payments/makePayment you can make a sandboxed $10 payment this then, upon success will redirect you to a confirmation page(unfinished), and more importantly Stripe will send a message back to our webhook to allow us to make a payment entry and probably update a reservation from pending to paid. For now though it just adds a payment under user id 1 until auth is set up. There is also a page at localhost:PORT/Payments/viewPayments that will show all payments currently in the database, when we set up auth I will switch it too only show the signed in users payments.
    In order to make it work securely I used local set dotnet user secrets so you will need to set those up for yourselves.
    Tutorial:
    initlize .NET user-secrets:
    dotnet user-secrets init
    
    I set up a sandboxed stripe account it was easy enough essentially go to https://dashboard.stripe.com/ and it should walk you through creating an account. I don't remeber the exact process good luck!
    
    When you get to the home page mine has a little recommendations box in the top right with my API keys a publishable key and a secret key. Either way if that isn't there go to settings search for API keys and under standard keys they are also there
    
    Once you have the keys you can go back to where you initlized the user-secrets and use the commands:
    dotnet user-secrets set "Stripe:SecretKey" "PASTE_KEY_HERE"
    dotnet user-secrets set "Stripe:PublishableKey" "PASTE_KEY_HERE"
    
    Then for this next step you'll need to check what port your server is
    running on mine ran on 5230 and I don't think yours will be different
    but if it is just throw run the server and check the logs to see the port
    once you have the port you'll also need to set up another secret to
    store it:
    dotnet user-secrets set "App:BaseUrl" "https://localhost:PORT"
    
    Once you do that because we are running, or at least I was testing on local host on windows, I needed to pass stripes webhooks back to the local host because stripe obviously can't access our local host so it apparently just communicates back to our machines which you can then if you're running stripe in listen mode pass back to the local host anyways, so you'll have to install the StripeCLI with:
    winget install Stripe.StripeCLI
    Once installed you'll need to login:
    stripe login
    The instructions are fairly self-explanatory so follow those and once you are directed to return to the CLI you can set up the listener with remeber to replace the port with yours:
    stripe listen --forward-to https://localhost:PORT/api/stripe/webhook
    once you start the listener it will display a webhook secret for you, we
    need to store this too:
    dotnet user-secrets set "Stripe:WebhookSecret" "Secret here!"
    
    This seems like a lot to me and I am not entirely sure why we doing this
    atleast for a school project but just to get used to the workflow and
    now accidently hardcoding security risks I decided to take this route
    because it was highly recomende to never commit secure keys and secrets

--- RVfamcamp/RVfamcamp/Program.cs
+++ RVfamcamp/RVfamcamp/Program.cs
@@ -1,11 +1,39 @@
+using RVfamcamp.Configuration;
+using RVfamcamp.Database;
+using RVfamcamp.Services;
+using Stripe;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.Configure<StripeSettings>(
+    builder.Configuration.GetSection("Stripe")
+);
+
+builder.Services.Configure<AppSettings>(
+	builder.Configuration.GetSection("App")
+);
+
+builder.Services.AddScoped<StripeService>();
+builder.Services.AddSingleton<PaymentRepo>(); // TODO: Changed back to AddScoped once I connect this Repo to the real data base
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+
+builder.Services.Configure<StripeSettings>(
+	builder.Configuration.GetSection("Stripe")
+);
 
 // For database
 AppDomain.CurrentDomain.SetData("DataDirectory", Directory.GetCurrentDirectory());
 var app = builder.Build();
+
+var stripeSettings =
+	builder.Configuration.GetSection("Stripe").Get<StripeSettings>();
+
+var appSettings = builder.Configuration.GetSection("app").Get<AppSettings>();
+
+StripeConfiguration.ApiKey = stripeSettings.SecretKey;
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -24,5 +52,7 @@ app.UseAuthorization();
 app.MapStaticAssets();
 app.MapRazorPages()
    .WithStaticAssets();
+
+app.MapControllers();
 
 app.Run();
